d0d59e1653e98c0a4b44ed229577ccda6e86874c
Tereza <tereza.halady@gmail.com>
2022-05-25 15:42:23 +0100

    Otevření souboru

--- src/com/engeto/Lekce5/ListOfPlants.java
+++ src/com/engeto/Lekce5/ListOfPlants.java
@@ -1,8 +1,10 @@
 package com.engeto.Lekce5;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+
 
 public class ListOfPlants {
     private List<Plant> seznamRostlin = new ArrayList<>();   //import List
@@ -17,5 +19,15 @@ public class ListOfPlants {
 
     // chybí metoda na získání rostliny na zadaném indexu
     // public String getRostlinu (Plant plant) {}
+    private Scanner x;
+
+    public void openFile(){
+        try {
+            x = new Scanner(new File("kvetiny.txt"));
+        }
+        catch (Exception e){
+            System.out.println("Soubor nelze otevřít");
+        }
+    }
 
 }
--- src/com/engeto/Lekce5/Main.java
+++ src/com/engeto/Lekce5/Main.java
@@ -1,8 +1,24 @@
 package com.engeto.Lekce5;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        File f = new File("kvetiny.txt");    // otevře soubor
+        try {
+            Scanner sc = new Scanner(f);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.err.println("Soubor nelze otevřít");
+            e.printStackTrace();
+        }
     }
+
+
 }
+
