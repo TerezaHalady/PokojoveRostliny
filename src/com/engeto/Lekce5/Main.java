4a82fca8801d87dd48f8bb541ce9384368c56da8
Tereza <tereza.halady@gmail.com>
2022-05-27 12:39:49 +0100

    ztroskotání

--- src/com/engeto/Lekce5/Main.java
+++ src/com/engeto/Lekce5/Main.java
@@ -1,24 +1,34 @@
 package com.engeto.Lekce5;
 
-import java.io.File;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.time.LocalDate;
 import java.util.Scanner;
+
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        File f = new File("kvetiny.txt");    // otevře soubor
-        try {
-            Scanner sc = new Scanner(f);
-            while (sc.hasNextLine()) {
-                System.out.println(sc.nextLine());
-            }
-        } catch (FileNotFoundException e) {
-            System.err.println("Soubor nelze otevřít");
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws IOException, PlantExceptions {
+//        String line = " ";
+//
+//        try {
+//            BufferedReader reader = new BufferedReader(new FileReader("kvetiny.txt"));   // přečte data
+//            while ((line = reader.readLine()) != null) {
+//                String[] values = line.split(";");         //rozdělí data dle rozdělovníku
+//                System.out.println("Název: " + values[0] +
+//                        ", naposledny zalito: " + values[3] +
+//                        ", frekvence zalívání " + values[2]);
+//
+//            }
+//
+//        } catch (FileNotFoundException e) {
+//            System.err.println("Soubor nelze otevřít");
+//            e.printStackTrace();
+//        }
+//        Plant p1 = new Plant("tlustice","řízek", LocalDate.now(),LocalDate.now(),25);
+//        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("kvetiny.txt"));
+//        oos.writeObject(p1.getWateringInfo());
+
+
+
     }
-
-
 }
-
